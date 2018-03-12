d1380864d4d7301fdcdbb38c26ba5e73bd9c8ba0
mradambeck <mr.adambeck@gmail.com>
2018-03-12 09:42:03 -1000

    trying to fix a weird off by one error on the dates that only appears on prod

--- models/Show.js
+++ models/Show.js
@@ -1,32 +1,32 @@
-var keystone = require('keystone'),
-    Types = keystone.Field.Types;
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
 
 var Show = new keystone.List('Show', {
-    autokey: { path: 'slug', from: 'eventDate venue location', unique: true },
-    map: { name: 'title' },
-    defaultSort: '-eventDate'
+  autokey: { path: 'slug', from: 'eventDate venue location', unique: true },
+  map: { name: 'title' },
+  defaultSort: '-eventDate'
 });
 
 Show.add({
-    eventDate: { type: Types.Datetime, default: Date.now, index: true },
+  eventDate: { type: Types.Datetime, default: Date.now, index: true },
 
-    venue: { type: String, required: true, default: "Yo Mama's House" },
-    streetAddress: {type: String, default: "1234 Adam Street"},
-    location: { type: String, required: true, default: "San Francisco, CA" },
+  venue: { type: String, required: true, default: 'Yo Mama\'s House' },
+  streetAddress: {type: String, default: '1234 Adam Street'},
+  location: { type: String, required: true, default: 'San Francisco, CA' },
 
-    title: { type: String, required: true },
+  title: { type: String, required: true },
 
-    ticketLink: String,
-    socialLink: String,
+  ticketLink: String,
+  socialLink: String,
 
-    state: { type: Types.Select, options: 'draft, published, archived', default: 'draft' },
-    author: { type: Types.Relationship, ref: 'User' },
-    createdAt: { type: Date, default: Date.now },
-    publishedAt: Date,
-    image: { type: Types.CloudinaryImage },
-    content: {
-        extended: { type: Types.Html, wysiwyg: true, height: 400 }
-    }
+  state: { type: Types.Select, options: 'draft, published, archived', default: 'draft' },
+  author: { type: Types.Relationship, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  publishedAt: Date,
+  image: { type: Types.CloudinaryImage },
+  content: {
+    extended: { type: Types.Html, wysiwyg: true, height: 400 }
+  }
 });
 
 Show.defaultColumns = 'eventDate|10%, venue|15%, title|25%, location|15%, state|10%, author|15%, publishedAt|10%';
--- routes/views/index.js
+++ routes/views/index.js
@@ -4,11 +4,10 @@ var async = require('async');
 
 exports = module.exports = function (req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
   var thisMorning = new Date();
-      thisMorning.setHours(0,0,0,0);
-
+  thisMorning.setHours(0, 0, 0, 0);
 
 
   locals.section = 'home';
@@ -20,45 +19,48 @@ exports = module.exports = function (req, res) {
 
   // Load UPCOMING SHOWS by date
   view.query('shows', keystone.list('Show').model.find()
-                              .where('state', 'published')
-                              .where('eventDate').gt(thisMorning)
-                              .sort('eventDate'));
+    .where('state', 'published')
+    .where('eventDate').gt(thisMorning)
+    .sort('eventDate'));
 
-  view.on('init', function(next){
+  view.on('init', function (next) {
     var shows = keystone.list('Show').model.find()
-                  .where('state', 'published')
-                  .where('eventDate').gt(thisMorning)
-                  .sort('eventDate');
-    shows.exec(function (err, results){
+      .where('state', 'published')
+      .where('eventDate').gt(thisMorning)
+      .sort('eventDate');
+    shows.exec(function (err, results) {
       locals.shows = results;
 
       // for Google indexing:
-      locals.showJSON = results.map(function(show){
+      locals.showJSON = results.map(function (show) {
         var showObj = {
           name: show.title,
           startDate: show.eventDate,
-          location:{
-            name:show.venue,
-            address:{
-              addressCountry:{
-                name:"US"
+          location: {
+            name: show.venue,
+            address: {
+              addressCountry: {
+                name: 'US'
               }
             }
           }
         };
 
         // because @ symbols break stuff:
-        showObj['@context'] = "http://schema.org";
-        showObj['@type'] = "MusicEvent";
-        showObj.location['@type'] = "Place";
-        showObj.location.address['@type'] = "PostalAddress";
+        showObj['@context'] = 'http://schema.org';
+        showObj['@type'] = 'MusicEvent';
+        showObj.location['@type'] = 'Place';
+        showObj.location.address['@type'] = 'PostalAddress';
 
-        if (show.ticketLink){ showObj.url = show.ticketLink; }
-        else if (show.socialLink){ showObj.url = show.socialLink; }
+        if (show.ticketLink) {
+          showObj.url = show.ticketLink;
+        } else if (show.socialLink) {
+          showObj.url = show.socialLink;
+        }
 
-        if (show.streetAddress){ showObj.location.address.streetAddress = show.streetAddress; }
-        if (show.location){ showObj.location.address.addressLocality = show.location; }
-        if (show.content.extended){showObj.description = show.content.extended; }
+        if (show.streetAddress) { showObj.location.address.streetAddress = show.streetAddress; }
+        if (show.location) { showObj.location.address.addressLocality = show.location; }
+        if (show.content.extended) { showObj.description = show.content.extended; }
 
         return JSON.stringify(showObj);
       });
@@ -67,26 +69,25 @@ exports = module.exports = function (req, res) {
     });
   });
 
-	// Load the posts
-	view.on('init', function (next) {
+  // Load the posts
+  view.on('init', function (next) {
 
-		var q = keystone.list('Post').paginate({
-			page: req.query.page || 1,
-			perPage: 10,
-			maxPages: 1,
-			filters: {
-				state: 'published',
-			},
-		})
-			.sort('-publishedDate');
+    var q = keystone.list('Post').paginate({
+      page: req.query.page || 1,
+      perPage: 10,
+      maxPages: 1,
+      filters: {
+        state: 'published'
+      }
+    }).sort('-publishedDate');
 
-		q.exec(function (err, results) {
-			locals.data.posts = results;
-			next(err);
-		});
-	});
+    q.exec(function (err, results) {
+      locals.data.posts = results;
+      next(err);
+    });
+  });
 
 
-	// Render the view
-	view.render('index');
+  // Render the view
+  view.render('index');
 };
